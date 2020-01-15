bece0dc639654f61e3e0194b6bd1af5d07c09892
836988 <836988@PC421410.cts.com>
2020-01-15 11:39:52 +0530

    My first level game

---
+++ src/game_pack/level1.java
@@ -0,0 +1,10 @@
+package game_pack;
+
+public class level1 {
+
+	public static void main(String[] args) {
+		System.out.println("My first level game project");// TODO Auto-generated method stub
+
+	}
+
+}
